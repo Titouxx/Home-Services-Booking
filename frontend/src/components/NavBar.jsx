bcc62b0314f7e927687b5ca73e1aa3d5630d4d60
Kelyan Taverny <taverny.k@gmail.com>
2025-05-19 15:29:17 +0200

    update

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
             <ul className="nav-links">
                 <li><a href="/auth/login">Who we are</a></li>
                 <li><a href="/auth/login">My profile</a></li>
-                <li><button className="basket">Basket (3)</button></li>
+                <li><button className="basket">Basket</button></li>
             </ul>
         </nav>
     );
