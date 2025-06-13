984283404d1bdd16ef02a1df2e1dfa4ef1863b11
Kelyan Taverny <taverny.k@gmail.com>
2025-06-13 19:47:00 +0200

    update

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/navbar.css";
+
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <Link to="/" style={{ textDecoration: "none", color: "#4c6920" }}>
+                    PLANITY
+                </Link>
+            </div>
+            <ul className="nav-links">
+                <li><a href="/templates/login.html">Who we are</a></li>
+                <li><a href="/templates/login.html">My profile</a></li>
+                <li>
+                    <Link to="/basket">
+                         <button className="basket">Basket</button>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
